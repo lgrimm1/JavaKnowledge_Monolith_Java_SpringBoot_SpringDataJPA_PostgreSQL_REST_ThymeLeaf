6c785356f73ca468b4eebc88fbe3381d78ab78ae
lgrimm1 <laszlo.grimm1@gmail.com>
2023-04-14 21:06:03 +0400

    Source template, desktop.js: moved textarea onload event script to template.

--- src/main/resources/static/js/desktop.js
+++ src/main/resources/static/js/desktop.js
@@ -1,8 +1,5 @@
 window.onload = function() {
     document.getElementById("titles_list").selectedIndex = 0;
-    var contentTextArea = document.getElementById("txt_content");
-    contentTextArea.focus();
-    contentTextArea.setSelectionRange(contentTextArea.value.length, contentTextArea.value.length);
 }
 
 function content_to_clipboard(id) {
